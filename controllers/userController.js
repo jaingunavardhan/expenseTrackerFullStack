39d08d6daf35adaf8acd908972ed183e20d30baf
jaingunavardhan <jaingunavardhan@gmail.com>
2024-03-26 02:28:16 +0530

    Signup & Login Implemented with Password hashing using bcrypt in DB

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const sequelize = require('../util/database.js');
 const User = require('../models/user.js');
-const { type } = require('os');
+const bcrypt = require('bcrypt');
 
 exports.getUsers = (request, response, next)=>{
     User.findAll()
@@ -12,51 +12,47 @@ exports.getUsers = (request, response, next)=>{
 
 exports.postUser = (request, response, next)=>{
     console.log("postUser...", request.body);
-    User.findAll()
+    User.findAll( {where:{email:request.body.email}} )
         .then(existingUsers=>{
-            console.log("existingUsers...", existingUsers);
-            for(var i=0; i<existingUsers.length; i++)
+            console.log("existingUsers...", existingUsers.length, existingUsers);
+            if(existingUsers.length>0)
             {
-                if(existingUsers[i].email == request.body.email)
-                {
-                    console.log("User Already Exits...");
-                    return response.json('User Already Exists...');
-                }
+                console.log("User Already Exists...");
+                return response.json('User Already Exists...');
             }
             console.log("User not exists... creating user...")
-            User.create({
-                username: request.body.username,
-                email: request.body.email,
-                password: request.body.password
-            })
-            .then(createdUser=>{
-                console.log("createdUser...", createdUser);
-                return response.json("Sign Up successful...");
-            })
+            
+            bcrypt.hash(request.body.password, 10, (error, hashData)=>{
+                User.create({
+                    username: request.body.username,
+                    email: request.body.email,
+                    password: hashData
+                })
+                .then(createdUser=>{
+                    console.log("createdUser...", createdUser);
+                    return response.json("Sign Up successful...");
+                })  
+            })         
         })
         .catch(error=>console.log(error));
 }
 
 exports.postLogin = (request, response, next)=>{
     console.log("postLogin...", request.body);
-    User.findAll()
+    User.findAll({where:{email:request.body.email}})
         .then(existingUsers=>{
-            let isEmail = false;
-            for(var i=0; i<existingUsers.length; i++)
+            console.log("Existing Users...", existingUsers.length, existingUsers);
+            if(existingUsers.length>0)
             {
-                if(existingUsers[i].email == request.body.email)
-                {
-                    isEmail = true;
-                    if(existingUsers[i].password == request.body.password)
+                bcrypt.compare(request.body.password, existingUsers[0].password, (error, result)=>{
+                    if(result)
                         return response.json("Logged In successfully...");
                     else
                         return response.status(401).json("Invalid Credentials...");
-                }
+                })                        
             }
-            if(isEmail == false)
-            {
+            else
                 return response.status(404).json("Email/User doesn't exist...");
-            }
         })
         .catch(error=>console.log(error));
 }
